c3965836ee6561f48ad0f975f45695e8249f1c25
Muhammad Hamza Farrukh <hamzafarrukh141@gmail.com>
2021-12-05 00:25:30 -0600

    Day 5 - Took too long

---
+++ d05/part2.py
@@ -0,0 +1,45 @@
+from typing import DefaultDict
+
+
+def readFile(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+
+lines = readFile('E:\\Projects\\AdventOfCode2021\\d05\\input.txt')
+
+floor = [[0 for _ in range(1000)] for _ in range(1000)]
+
+
+for l in lines:
+    l = l.split(' -> ')
+    x1 = int(l[0].split(',')[0])
+    y1 = int(l[0].split(',')[1])
+    x2 = int(l[1].split(',')[0])
+    y2 = int(l[1].split(',')[1])
+    if x1 == x2:
+        for i in range(min(y1, y2), max(y1, y2)+1):
+            floor[x1][i] += 1
+    elif y1 == y2:
+        for i in range(min(x1, x2), max(x1, x2)+1):
+            floor[i][y1] += 1
+    elif abs(x1 - x2) == abs(y1 - y2):
+        x_dir = 1
+        y_dir = 1
+        if x1 > x2:
+            x_dir = -1
+        if y1 > y2:
+            y_dir = -1
+        for i in range(abs(x1 - x2) + 1):
+            floor[x1 + (x_dir * i)][y1 + (y_dir * i)] += 1
+
+count = 0
+for i in range(len(floor)):
+    for j in range(len(floor[0])):
+        if floor[i][j] > 1:
+            count += 1
+
+print(count)
