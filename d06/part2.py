58c65d81c5f398ae5f7a0abc6042dc4fb3863c55
Muhammad Hamza Farrukh <hamzafarrukh141@gmail.com>
2021-12-06 00:10:20 -0600

    Day 6 - Fish or Rabbits

---
+++ d06/part2.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+from os import fsdecode
+
+
+def readFile(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+
+lines = readFile('E:\\Projects\\AdventOfCode2021\\d06\\input.txt')
+
+fishies = [int(f) for f in lines[0].split(',')]
+fish = defaultdict(int)
+for f in fishies:
+    fish[f] += 1
+
+for day in range(256):
+    growth = fish[0]
+    for f in range(1, 9):
+        fish[f - 1] = fish[f]
+    fish[8] = growth
+    fish[6] += growth
+
+print(sum(fish.values()))
