6dfce3804a05dabc349cccb5b9c38ea7570dae72
Muhammad Hamza Farrukh <hamzafarrukh141@gmail.com>
2021-12-06 23:30:17 -0600

    Day 7 - Greedy way worked

---
+++ d07/part1.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+import sys
+
+
+def readFile(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+
+lines = readFile('E:\\Projects\\AdventOfCode2021\\d07\\input.txt')
+
+crabs = [int(c) for c in lines[0].split(',')]
+
+counts = defaultdict(int)
+
+for c in crabs:
+    counts[c] += 1
+
+min_fuel = sys.maxsize
+for i in counts.keys():
+    new_fuel = 0
+    for j in counts.keys():
+        c = counts[j]
+        new_fuel += (abs(i-j) * c)
+    if new_fuel < min_fuel:
+        min_fuel = new_fuel
+
+
+print(min_fuel)
