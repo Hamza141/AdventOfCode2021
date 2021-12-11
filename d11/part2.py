dcbb811c6b7725431c04fd7059597c8673ae25a8
Muhammad Hamza Farrukh <hamzafarrukh141@gmail.com>
2021-12-10 23:35:21 -0600

    Day 11 - That was quick again

---
+++ d11/part2.py
@@ -0,0 +1,61 @@
+def readFile(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+
+lines = readFile('E:\\Projects\\AdventOfCode2021\\d11\\input.txt')
+
+grid = [[0 for _ in range(len(lines[0]))] for _ in range(len(lines))]
+
+for i in range(len(lines)):
+    for j in range(len(lines[0])):
+        grid[i][j] = int(lines[i][j])
+
+step = 1
+while True:
+    for i in range(len(lines)):
+        for j in range(len(lines[0])):
+            grid[i][j] += 1
+
+    flash = [[False for _ in range(len(lines[0]))] for _ in range(len(lines))]
+    left = True
+    while left:
+        left = False
+        for i in range(len(lines)):
+            for j in range(len(lines[0])):
+                if grid[i][j] > 9 and not flash[i][j]:
+                    flash[i][j] = True
+                    if i - 1 >= 0:
+                        grid[i-1][j] += 1
+                    if i + 1 < 10:
+                        grid[i+1][j] += 1
+                    if j - 1 >= 0:
+                        grid[i][j-1] += 1
+                    if j + 1 < 10:
+                        grid[i][j+1] += 1
+                    if i - 1 >= 0 and j - 1 >= 0:
+                        grid[i-1][j-1] += 1
+                    if i + 1 < 10 and j + 1 < 10:
+                        grid[i+1][j+1] += 1
+                    if j - 1 >= 0 and i + 1 < 10:
+                        grid[i + 1][j-1] += 1
+                    if j + 1 < 10 and i - 1 >= 0:
+                        grid[i-1][j+1] += 1
+            for i in range(len(lines)):
+                for j in range(len(lines[0])):
+                    if grid[i][j] > 9 and not flash[i][j]:
+                        left = True
+    flashes = 0
+    for i in range(len(lines)):
+        for j in range(len(lines[0])):
+            if grid[i][j] > 9:
+                flashes += 1
+                grid[i][j] = 0
+    if flashes == 100:
+        break
+    step += 1
+
+print(step)
